43a24cd8ef62d6e392d7829338731a278924d827
Fernando Demarchi Natividade Luiz <nativanando@gmail.com>
2019-05-02 15:47:34 -0300

    fixing the script on running the container

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t webodm_power_image .
-docker run -tid -P --name webodm_power webodm_power_image
+docker run -tid -p 8085:8000 --name webodm_power webodm_power_image
 docker exec webodm_power docker-entrypoint.sh
